8fa36b3b980d974f81b29ee4e23a593e218239c3
Patrick Gundlach <gundlach@speedata.de>
2013-01-15 11:49:42 +0100

    Fix typo, no changes

--- lua-check-hyphen.lua
+++ lua-check-hyphen.lua
@@ -20,24 +20,24 @@ local explode = function(s,p)
    return t
 end
 
-luachekchyphen = {}
+luacheckhyphen = {}
 
 
-luachekchyphen.hyphenattr = luatexbase.new_attribute("hyphenattr")
-luachekchyphen.hyphenwords = {}
-luachekchyphen.all_hyphenatedwords = {}
-luachekchyphen.word_whitelist = {}
-luachekchyphen.rectangle = node.new("whatsit","pdf_literal")
-luachekchyphen.rectangle.data = string.format("q 0 0 10 10 re f S Q")
+luacheckhyphen.hyphenattr = luatexbase.new_attribute("hyphenattr")
+luacheckhyphen.hyphenwords = {}
+luacheckhyphen.all_hyphenatedwords = {}
+luacheckhyphen.word_whitelist = {}
+luacheckhyphen.rectangle = node.new("whatsit","pdf_literal")
+luacheckhyphen.rectangle.data = string.format("q 0 0 10 10 re f S Q")
 
-luachekchyphen.deligature = function ( glyph_node )
+luacheckhyphen.deligature = function ( glyph_node )
   local sln = unicode.utf8
   local head = glyph_node.components
   local str = ""
   while head do
     if head.id == 37 then
       if head.components then
-        str = str .. luachekchyphen.deligature(head)
+        str = str .. luacheckhyphen.deligature(head)
       else
         str = str .. sln.char(head.char)
       end
@@ -48,14 +48,14 @@ luachekchyphen.deligature = function ( glyph_node )
 end
 
 
-luachekchyphen.collect_discs = function(head)
+luacheckhyphen.collect_discs = function(head)
   local word_start
   local word
   -- this is where we store all the breakpoints
   local thisbreakpoint
   local word_with_hyphen
   local c
-  local hyphencounter = #luachekchyphen.hyphenwords + 1
+  local hyphencounter = #luacheckhyphen.hyphenwords + 1
   local sln = unicode.utf8
   local ligature_chars
   while head do
@@ -71,11 +71,11 @@ luachekchyphen.collect_discs = function(head)
       while word_start and word_start.id ~= 10 do
         if word_start == head then -- disc
           -- there is a breakpoint after letter c
-          node.set_attribute(head,luachekchyphen.hyphenattr,hyphencounter)
+          node.set_attribute(head,luacheckhyphen.hyphenattr,hyphencounter)
           thisbreakpoint = c
         elseif word_start.id == 37 then
           if word_start.components then
-            ligature_chars = luachekchyphen.deligature(word_start)
+            ligature_chars = luacheckhyphen.deligature(word_start)
             word = word .. ligature_chars
             c = c + string.len(ligature_chars)
           elseif sln.match(sln.char(word_start.char),"%a") then
@@ -87,8 +87,8 @@ luachekchyphen.collect_discs = function(head)
       end
       if thisbreakpoint then
         word_with_hyphen = sln.sub(word,1,thisbreakpoint) .. "-" .. sln.sub(word,thisbreakpoint+1,-1)
-        luachekchyphen.hyphenwords[hyphencounter] = word_with_hyphen
-        hyphencounter = #luachekchyphen.hyphenwords + 1
+        luacheckhyphen.hyphenwords[hyphencounter] = word_with_hyphen
+        hyphencounter = #luacheckhyphen.hyphenwords + 1
       end
     end
     head = head.next
@@ -96,22 +96,22 @@ luachekchyphen.collect_discs = function(head)
   return true
 end
 
-luachekchyphen.check_discs = function (head,parent)
+luacheckhyphen.check_discs = function (head,parent)
   local c
   local word
   local tmp
   while head do
     if head.id < 2 then
-      luachekchyphen.check_discs(head.list,head)
+      luacheckhyphen.check_discs(head.list,head)
     elseif head.id == 7 and head.next and head.next.id == 10 then -- disc
-      c = node.has_attribute(head,luachekchyphen.hyphenattr)
-      word = luachekchyphen.hyphenwords[c]
-      if luachekchyphen.word_whitelist[word] then
+      c = node.has_attribute(head,luacheckhyphen.hyphenattr)
+      word = luacheckhyphen.hyphenwords[c]
+      if luacheckhyphen.word_whitelist[word] then
       	  -- word found, but OK (whitelisted)
       else
-	      luachekchyphen.all_hyphenatedwords[word] = true
-     	  if luachekchyphen.drawmarks then
-     	  	tmp = node.copy(luachekchyphen.rectangle)
+	      luacheckhyphen.all_hyphenatedwords[word] = true
+     	  if luacheckhyphen.drawmarks then
+     	  	tmp = node.copy(luacheckhyphen.rectangle)
      	  	node.insert_after(parent,head,tmp)
      	  end
 	  end
@@ -121,54 +121,54 @@ luachekchyphen.check_discs = function (head,parent)
   return true
 end
 
-luachekchyphen.listhyphenatedwords = function()
-	if luachekchyphen.final == "true" then
+luacheckhyphen.listhyphenatedwords = function()
+	if luacheckhyphen.final == "true" then
 		return
 	end
   -- don't write if the use has turned that off!
-  if not luachekchyphen.nofile then
+  if not luacheckhyphen.nofile then
     local unknown_hyphenation_filename = tex.jobname .. ".uhy"
     local unknown_hyphenation_file = io.open(unknown_hyphenation_filename,"w")
   
-    for k,v in pairs(luachekchyphen.all_hyphenatedwords) do
+    for k,v in pairs(luacheckhyphen.all_hyphenatedwords) do
     	unknown_hyphenation_file:write(k .. "\n")
     end
     unknown_hyphenation_file:close()
   end
 
   texio.write_nl("log","All words with unknown hyphenation below")
-  for k,v in pairs(luachekchyphen.all_hyphenatedwords) do
+  for k,v in pairs(luacheckhyphen.all_hyphenatedwords) do
       texio.write_nl("log",k)
   end
 
 end
 
-luachekchyphen.enable = function()
-	if luachekchyphen.final == "true" then
+luacheckhyphen.enable = function()
+	if luacheckhyphen.final == "true" then
 		return
 	end
   	local whitelistfile,err
   	local filecontents
-  	if luachekchyphen.whitelist then
-  		for i,v in ipairs(string.explode(luachekchyphen.whitelist,",")) do
+  	if luacheckhyphen.whitelist then
+  		for i,v in ipairs(string.explode(luacheckhyphen.whitelist,",")) do
   			whitelistfile,err = io.open(v)
   			if not whitelistfile then
   				texio.write_nl(err)
 	  		else
   				filecontents = whitelistfile:read("*a")
   				for _,entry in ipairs(explode(filecontents,"[^%s]+")) do
-  					luachekchyphen.word_whitelist[entry] = true
+  					luacheckhyphen.word_whitelist[entry] = true
 	  			end
   			end
   		end
  	end
-  	if luachekchyphen.mark == "true" then
-  		luachekchyphen.drawmarks = true
+  	if luacheckhyphen.mark == "true" then
+  		luacheckhyphen.drawmarks = true
   	end
-    luatexbase.add_to_callback("pre_linebreak_filter", luachekchyphen.collect_discs,"collect_discs")
-    luatexbase.add_to_callback("post_linebreak_filter",luachekchyphen.check_discs,"check_discs")
+    luatexbase.add_to_callback("pre_linebreak_filter", luacheckhyphen.collect_discs,"collect_discs")
+    luatexbase.add_to_callback("post_linebreak_filter",luacheckhyphen.check_discs,"check_discs")
 end
 
-return luachekchyphen
+return luacheckhyphen
 
 -- end of file
